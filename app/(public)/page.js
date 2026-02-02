2532acda85f5365f9a5aac6def72073ec36c171f
PranoyAcharyya <acharyapranoy@gmail.com>
2026-02-02 16:09:52 +0530

    push

--- app/(public)/page.js
+++ app/(public)/page.js
@@ -2,15 +2,19 @@
 import React from 'react'
 import Image from 'next/image'
 import { VideoText } from "../../components/ui/video-text"
+import { AuroraText } from '@/components/ui/aurora-text'
 
 
 const page = () => {
   return (
-    <section className="container mx-auto px-4 pt-[100px] lg:h-128 lg:flex lg:items-center lg:space-x-8">
+    <section className=" flex flex-col container mx-auto px-4 pt-[100px] lg:h-128 lg:flex lg:items-center lg:space-x-8">
     
           <div className="relative h-[500px] w-full overflow-hidden">
   <VideoText src="https://cdn.magicui.design/ocean-small.webm">MIND</VideoText>
 </div>
+<h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl text-capitalize">
+      Write <AuroraText className="capitalize">Whatever In your mind</AuroraText>
+    </h1>
     
         </section>
   )
