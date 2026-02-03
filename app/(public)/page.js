b056834ea6d5c75415bb69c166cc45aa4d6a55ca
PranoyAcharyya <acharyapranoy@gmail.com>
2026-02-03 19:07:50 +0530

    push

--- app/(public)/page.js
+++ app/(public)/page.js
@@ -2,16 +2,16 @@
 import React from 'react'
 import { VideoText } from "../../components/ui/video-text"
 import { AuroraText } from '@/components/ui/aurora-text'
-import { ScrollVelocityContainer, ScrollVelocityRow } from '@/components/ui/scroll-based-velocity'
+// import videobg from "../../public/textbg.mp4"
 
 
 const page = () => {
   return (<>
 
-    <section className=" flex flex-col container mx-auto px-4 pt-[100px] lg:h-128 lg:flex lg:items-center lg:space-x-8">
-    
+    <section className="relative flex flex-col container mx-auto px-4 pt-[100px] lg:h-128 lg:flex lg:items-center lg:space-x-8">
+         
           <div className="relative h-[200px] md:h-[500px] w-full overflow-hidden">
-  <VideoText src="https://cdn.magicui.design/ocean-small.webm">MIND</VideoText>
+  <VideoText src="/textbg2.mp4">MIND</VideoText>
 </div>
 <h1 className="text-4xl text-center font-bold tracking-tighter md:text-5xl lg:text-7xl text-capitalize">
       Write <AuroraText className="capitalize">Whatever In your mind</AuroraText>
