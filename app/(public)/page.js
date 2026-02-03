5c1a2ee7addd4cd51d818583c32769afdd539102
PranoyAcharyya <acharyapranoy@gmail.com>
2026-02-03 18:08:49 +0530

    push

--- app/(public)/page.js
+++ app/(public)/page.js
@@ -19,18 +19,7 @@ const page = () => {
     
         </section>
         <section className='py-20'>
-        <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-      <ScrollVelocityContainer className="text-4xl font-bold tracking-[-0.02em] md:text-7xl md:leading-[5rem]">
-        <ScrollVelocityRow baseVelocity={20} direction={1}>
-          Developent Blogs 
-        </ScrollVelocityRow>
-        <ScrollVelocityRow baseVelocity={20} direction={-1}>
-          Blogs
-        </ScrollVelocityRow>
-      </ScrollVelocityContainer>
-      <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r"></div>
-      <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l"></div>
-    </div>
+        
         </section>
 
         
